8f04b1ed41dbf86ecc76313c78191c0e7b90a045
wangyue <wangyue@systemdeMacBook-Air.local>
2015-07-29 23:25:38 +0800

    初始化test1

---
+++ test1/test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by wangyue on 15/7/29.
+//  Copyright (c) 2015年 system . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test1/test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test1
+//
+//  Created by wangyue on 15/7/29.
+//  Copyright (c) 2015年 system . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
